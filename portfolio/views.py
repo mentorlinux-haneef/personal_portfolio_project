2c988c91ccc878f03cee335e2c0b8eaf53260956
MohamedHaneef <haneef@mentorlinux.com>
2021-04-11 13:32:02 +0400

    portfolio DB link established

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
+
+def home(request):
+    pobj = Project.objects.all()
+    return render(request, 'portfolio/home.html', {'pobj':pobj})
